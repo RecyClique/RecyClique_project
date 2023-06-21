421187880ae7a6e6a8501c9f72de93c002264b3c
Staceyannking <staceyannking01@gmail.com>
2023-06-21 19:21:52 -0400

    dashboard is able to list all the events

--- frontend/src/pages/Dashboard.jsx
+++ frontend/src/pages/Dashboard.jsx
@@ -44,26 +44,64 @@ const Dashboard = () => {
 
       <div className="tabs is-centered">
         <ul>
-          <li className={currentTab === 0 ? "is-active" : ""} onClick={() => changeTab(0)}><a>Current Events</a></li>
-          <li className={currentTab === 1 ? "is-active" : ""} onClick={() => changeTab(1)}><a>Past Events</a></li>
+          <li className={currentTab === 0 ? "is-active" : ""} onClick={() => changeTab(0)}><a>Joined Events</a></li>
+          {/* <li className={currentTab === 1 ? "is-active" : ""} onClick={() => changeTab(1)}><a>Past Events</a></li> */}
           <li className={currentTab === 2 ? "is-active" : ""} onClick={() => changeTab(2)}><a>Events Created</a></li>
         </ul>
       </div>
       <div className="tab-content">
         <div className={currentTab !== 0 ? "is-hidden" : ""}>
-
+           
+        {
+            joined ? joined[0].map(joinedEvent => {
+              console.log('result2' + joinedEvent)
+              return (
+                <>
+                  <div className='box eventBox' id={'eventId: ' + joinedEvent.id}>
+                      <div>
+                        <h1 className='title'>{joinedEvent.title}</h1>
+                        <p>{joinedEvent.borough}</p>
+                        <p>{joinedEvent.location}</p>
+                        <p>{joinedEvent.start_date === joinedEvent.end_date ? joinedEvent.start_date.substring(0, 10) : joinedEvent.start_date.substring(0, 10) + ' - ' + joinedEvent.end_date.substring(0, 10)}</p>
+                        <p>{joinedEvent.start_time + ' - ' + joinedEvent.end_time}</p>
+                      </div>
+                      <div className='cardSec2'>
+                        <button className='button is-danger'>Leave Event</button>
+                      </div>
+                      <div>
+                        <h1 className='is-size-5 has-text-weight-bold mt-4'>Description</h1>
+                        <p>{joinedEvent.description}</p>
+                      </div>
+                    </div>
+                </>
+              )
+            }) : 'NAY'
+          }
         </div>
-        <div className={currentTab !== 1 ? "is-hidden" : ""}>
+        {/* <div className={currentTab !== 1 ? "is-hidden" : ""}>
           <p>test2</p>
-        </div>
+        </div> */}
         <div className={currentTab !== 2 ? "is-hidden" : ""}>
           {
             created ? created[0].map(createdEvent => {
-              console.log('test' + createdEvent)
+              console.log('result1' + createdEvent)
               return (
-                <>
-                  <p>{createdEvent.title}</p>
-                </>
+                <div className='box eventBox' id={'eventId: ' + createdEvent.id}>
+                      <div>
+                        <h1 className='title'>{createdEvent.title}</h1>
+                        <p>{createdEvent.borough}</p>
+                        <p>{createdEvent.location}</p>
+                        <p>{createdEvent.start_date === createdEvent.end_date ? createdEvent.start_date.substring(0, 10) : createdEvent.start_date.substring(0, 10) + ' - ' + createdEvent.end_date.substring(0, 10)}</p>
+                        <p>{createdEvent.start_time + ' - ' + createdEvent.end_time}</p>
+                      </div>
+                      <div className='cardSec2'>
+                        <button className='button is-primary'>Delete Event</button>
+                      </div>
+                      <div>
+                        <h1 className='is-size-5 has-text-weight-bold mt-4'>Description</h1>
+                        <p>{createdEvent.description}</p>
+                      </div>
+                    </div>
               )
             }) : 'NAY'
           }
