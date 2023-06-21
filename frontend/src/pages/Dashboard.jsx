60449921981915e46547b86d9e130d0c51790c34
OrwRandal <randypichardo1987@gmail.com>
2023-06-21 12:22:41 -0400

    finished fetching for dashboard

--- frontend/src/pages/Dashboard.jsx
+++ frontend/src/pages/Dashboard.jsx
@@ -19,44 +19,57 @@ const Dashboard = () => {
     };
 
     fetchData();
+    // console.log(created? created[0]: null);
+    // console.log(joined? joined[0]: null);
   }, [currentUser]);
 
-  console.log(joined);
-  console.log(created);
+  useEffect(() => {
+    console.log(created ? created[0] : null);
+    console.log(joined ? joined[0] : null);
+  }, [created, joined]);
 
-  const [currentTab , setCurrentTab] = useState(0);
-const changeTab = (index) => {
-  setCurrentTab(index)
-}
+  const [currentTab, setCurrentTab] = useState(0);
+  const changeTab = (index) => {
+    setCurrentTab(index)
+  }
 
-  
+
   return (
     <>
-    <div>
-      <h1>Dashboard Page</h1>
-    </div>
-  
+      <div>
+        <h1>Dashboard Page</h1>
+      </div>
 
 
- <div className="tabs is-centered">
-  <ul>
-    <li className={currentTab === 0 ? "is-active": ""  }onClick={() => changeTab(0)}><a>Current Events</a></li>
-    <li className={currentTab === 1 ? "is-active": ""  }onClick={() => changeTab(1)}><a>Past Events</a></li>
-    <li className={currentTab === 2 ? "is-active": ""  }onClick={() => changeTab(2)}><a>Events Created</a></li>
-  </ul>
-</div>
-<div className="tab-content">
-    <div className={currentTab !== 0 ? "is-hidden": ""  }>
-      
+
+      <div className="tabs is-centered">
+        <ul>
+          <li className={currentTab === 0 ? "is-active" : ""} onClick={() => changeTab(0)}><a>Current Events</a></li>
+          <li className={currentTab === 1 ? "is-active" : ""} onClick={() => changeTab(1)}><a>Past Events</a></li>
+          <li className={currentTab === 2 ? "is-active" : ""} onClick={() => changeTab(2)}><a>Events Created</a></li>
+        </ul>
       </div>
-    <div className={currentTab !== 1 ? "is-hidden": ""  }>
-      <p>test2</p>
+      <div className="tab-content">
+        <div className={currentTab !== 0 ? "is-hidden" : ""}>
+
+        </div>
+        <div className={currentTab !== 1 ? "is-hidden" : ""}>
+          <p>test2</p>
+        </div>
+        <div className={currentTab !== 2 ? "is-hidden" : ""}>
+          {
+            created ? created[0].map(createdEvent => {
+              console.log('test' + createdEvent)
+              return (
+                <>
+                  <p>{createdEvent.title}</p>
+                </>
+              )
+            }) : 'NAY'
+          }
+        </div>
       </div>
-    <div className={currentTab !== 2 ? "is-hidden": ""  }>
-      
-      </div>
-  </div>
-  </>
+    </>
   )
 }
 
